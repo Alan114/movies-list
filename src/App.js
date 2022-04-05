43d970b508e4c1b388a1941a14e5f3dd86570b8a
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-05 15:24:16 +0200

    Handling http errors by using try catch and throwing Error and showing it conditionally

--- src/App.js
+++ src/App.js
@@ -6,21 +6,32 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.py4e.com/api/films/");
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/films/");
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      if (!response.ok) {
+        throw Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
 
@@ -30,9 +41,10 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {isLoading && movies.length > 0 && <p>Loading...</p>}
-        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
-        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
